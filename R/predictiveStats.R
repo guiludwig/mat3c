d54f1041bf0cd886d5bb31769b76f4a7a7524be6
guiludwig <guilhermeludwig@gmail.com>
2018-11-19 22:11:05 -0200

    predictiveStats refer to Andersson et al 2016

--- R/predictiveStats.R
+++ R/predictiveStats.R
@@ -1,4 +1,23 @@
+#' Predictive Analysis for a fitmat3 object
+#'
+#' Evaluates the predictive posterior probability of T > T_obs for various statistics.
+#'
+#' @param model An fitmat3 object (resulting from a call to fitmat3)
+#' @param fname If the MCMC samples were saved into text files, this can be used to generate
+#'              predictive probabilities from them. Defaults to NULL.
+#' @param whichSamples Which (and how many) samples to collect in order to generate the
+#'              predictive probabilities. Must be smaller than the number of MCMC samples.
+#' @param additionalArgs additional arguments to be passed to rmat3() (if read from text files,
+#'              it is necessary to provide a list with window, R and R_c).
+#'
 #' @export
+#' @return \code{predictiveStats} returns a list containing at least the following components
+#'   \item{pL}{predictive probability for the expected number of centers}
+#'   \item{pRc}{predictive probability for the number of fingers within R_centers distance of other centers}
+#'   \item{pnx}{predictive probability for the average number of fingers}
+#'   \item{psx}{predictive probability for the standard deviation of the number of fingers}
+#'   \item{chx}{predictive probability for the mean area of the convex hull of fingers (reactive territory)}
+#'   \item{spx}{predictive probability for the spanned angle of fingers}
 predictiveStats <- function(model, fname = NULL, whichSamples, additionalArgs = list(win = owin(),
                                                                                       R = 0.005,
                                                                                       R_c = 0.02), ...){
@@ -35,12 +54,18 @@ predictiveStats <- function(model, fname = NULL, whichSamples, additionalArgs = 
   suff[2] <- -1*suff[2]
   n.fingers <- mean(sapply(fmat.cond, function(x) length(x$x)))
   s.fingers <- sd(sapply(fmat.cond, function(x) length(x$x)))
-  L <- length(whichSamples)
-  suff0 <- matrix(0, ncol = 2, nrow = L)
-  n.fingers0 <- numeric(L)
-  s.fingers0 <- numeric(L)
+  mchull <- mean(sapply(fmat.cond, function(x) areahull(cbind(x$x, x$y))))
+  spangle <- mean(sapply(fmat.cond, spanAngle))
+
+  M <- length(whichSamples)
+  suff0 <- matrix(0, ncol = 2, nrow = M)
+  n.fingers0 <- numeric(M)
+  s.fingers0 <- numeric(M)
+  mchull0 <- numeric(M)
+  spangle0 <- numeric(M)
+
   for(i in seq_along(whichSamples)){
-    cat(sprintf("%3.1f\n", 100*i/L))
+    cat(sprintf("%3.1f\n", 100*i/M))
     x <- rmat3(beta  = parameters[whichSamples[i], "beta"],
                phi   = parameters[whichSamples[i], "phi"],
                gamma = parameters[whichSamples[i], "gamma"],
@@ -52,14 +77,21 @@ predictiveStats <- function(model, fname = NULL, whichSamples, additionalArgs = 
     suff0[i, ] <- sufficientStat(x, R_c)
     n.fingers0[i] <- mean(sapply(x, function(x) nrow(x$fingers)))
     s.fingers0[i] <- sd(sapply(x, function(x) nrow(x$fingers)))
+    mchull0[i] <- mean(sapply(x, function(x) areahull(cbind(x$x, x$y))))
+    spangle0[i] <- mean(sapply(fmat.cond, spanAngle))
   }
   suff0[,2] <- -1*suff0[,2]
-  psuff1 <- sum(suff[1] < suff0[,1])/L
-  psuff2 <- sum(suff[2] < suff0[,2])/L
-  pn <- sum(n.fingers < n.fingers0)/L
-  ps <- sum(s.fingers < s.fingers0)/L
+  psuff1 <- sum(suff[1] < suff0[,1])/M
+  psuff2 <- sum(suff[2] < suff0[,2])/M
+  pn <- sum(n.fingers < n.fingers0)/M
+  ps <- sum(s.fingers < s.fingers0)/M
+  ch <- sum(mchull < mchull0)/M
+  sp <- sum(spangle < spangle0)/M
+
   return(list(pL = psuff1,
               pRc = psuff2,
               pnx = pn,
-              psx = ps))
+              psx = ps,
+              chx = ch,
+              spx = sp))
 }
